4a591c565bc6f349c3c704aa8ae325a13cadc6c5
RoboPhred <robophred@gmail.com>
2022-06-14 16:57:14 -0700

    Do not use path history on github

--- src/services/history/History.ts
+++ src/services/history/History.ts
@@ -9,9 +9,9 @@ export const History: Identifier<HistoryType> = "history";
 // eslint-disable-next-line @typescript-eslint/no-redeclare -- Merged DI identifier.
 export type History = HistoryType;
 export function historyFactory() {
-  if (process.env.NODE_ENV === "production") {
-    return createBrowserHistory();
-  } else {
-    return createHashHistory();
-  }
+  // if (process.env.NODE_ENV === "production") {
+  //   return createBrowserHistory();
+  // } else {
+  return createHashHistory();
+  // }
 }
